691e67e85b8e602506122831ac0d4856c212d77f
Dule <crashendodt@gmail.com>
2019-11-15 11:30:53 +0100

    adjusted height break points

--- src/index.js
+++ src/index.js
@@ -5,5 +5,7 @@ import './styles/styles.scss';
 import './styles/breakpoint1200.scss';
 import './styles/breakpoint900.scss';
 import './styles/breakpoint768.scss';
+import './styles/breakpoint600.scss';
+import './styles/breakpoint_height.scss';
 
 ReactDOM.render(<App />, document.getElementById('root'));
